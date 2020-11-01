31dbb806ac89854f0fdf52378d86662fc60e48e3
CelestaB <65030349+CelestaB@users.noreply.github.com>
2020-10-31 21:04:53 -0500

    first import
    first update

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("your index loaded correctly");
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  };
+  
